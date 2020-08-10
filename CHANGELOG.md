eda4890a431b8b78e0360adf0dabd9d3e640bec5
AlbertAZ1992 <ziyuximing@163.com>
2020-08-10 12:11:57 +0800

    chore: update release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+### [1.0.6](https://github.com/AlbertAZ1992/react-simple-clamp/compare/v1.0.5...v1.0.6) (2020-08-10)
+
 ### [1.0.5](https://github.com/AlbertAZ1992/react-simple-clamp/compare/v1.0.4...v1.0.5) (2020-08-10)
 
 ### 1.0.4 (2020-08-10)
--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -14,67 +14,6 @@ import pkg from '../package.json';
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
-// const devSite = `http://127.0.0.1:${PORT}`;
-// const devPath = path.resolve('example', 'index.html');
-// const devUrl = `${devSite}/${devPath}`;
-
-// const options = {
-//   input: 'src/index.tsx',
-//   output: [
-//     {
-//       file: path.resolve(__dirname, '..', pkg.main),
-//       format: 'umd',
-//       name: 'ReactSimpleClamp',
-//     },
-//     {
-//       file: path.resolve(__dirname, '..', pkg.module),
-//       format: 'es',
-//       isExternal: true,
-//     },
-//     {
-//       file: path.resolve(__dirname, '..', pkg.unpkg),
-//       format: 'iife',
-//       name: 'ReactSimpleClamp',
-//       isUglify: true,
-//       // plugins: [terser()],
-//     },
-//   ],
-//   external: ['react', 'react-dom'],
-//   globals: {
-//     'react': 'React',
-//     'react-dom': 'ReactDOM'
-//   },
-//   plugins: [
-//     json(),
-//     nodeResolve({ extensions, browser: true }),
-//     commonjs(),
-//     clear({
-//       targets: ['dist', 'es', 'lib'],
-//     }),
-//     postcss({
-//       extract: false,
-//       minimize: true,
-//       extensions: ['.css', '.less'],
-//       plugins: [autoprefixer],
-//     }),
-//     typescript(),
-//     sucrase({
-//       exclude: ['node_modules/**'],
-//       transforms: ['typescript', 'jsx'],
-//     }),
-//     babel({
-//       extensions,
-//       include: ['src/**/*', 'example/**/*'],
-//       exclude: 'node_modules/**',
-//       babelHelpers: 'runtime',
-//     }),
-//     progress({ clearLine: false }),
-//   ],
-// };
-
-// export default options;
-
-// const isDev = process.env.NODE_ENV !== 'production';
 const outputs = [
   {
     file: path.resolve(__dirname, '..', pkg.main),
